4552861e86dea742c1820ebeb44fc8d956b9beca
Doug Duncan <duncan.doug@gmail.com>
2023-05-06 19:58:36 -0500

    update w/ f-strings

--- Milestone_1_Immutable_Objects.py
+++ Milestone_1_Immutable_Objects.py
@@ -11,39 +11,39 @@ a = 1
 b = a
 # Change one of the vars and see what happens
 b = 2
-print('Binding for b has changed from 1 to 2--> id(a) =',id(a),a,'id(b) =',id(b),b)  
+print(f'Binding for b has changed from {a} to {b} --> id(a) = {id(a)} and a = {a}, id(b) = {id(b)} and b = {b}')  
 
 # float test (immutable object)
 c = 1.1
 d = c
 # Change one of the vars to see what happens
 d = 2.1
-print('Binding for d has changed from 1.1 to 2.1--> id(c) =',id(c),c,'id(d)=',id(d),d)  
+print(f'Binding for d has changed from {c} to {d} --> id(c) = {id(c)} and c = {c}, id(d) = {id(d)} and d = {d}')  
 
 # str test (immutable object)
 e = 'e'
 f = e
 f = 'f'
-print('Binding for f has changed from "e" to "f"--> id(e)=',id(e), e,'id(f)= ', id(f),f)
+print(f'Binding for f has changed from {e} to {f} --> id(e) = {id(e)} and e = {e}, id(f)= {id(f)} and f = {f}')
 
 #list test (mutable object)
 g = [1,2,3]
 h = g
 h[1] *= 100
-print('Binding for h is the same as the binding for g--> id(g)=',id(g), g,'id(h) =',id(h),h)
+print(f'Binding for h is the same as the binding for g--> id(g)= {id(g)} and g = {g}, id(h) = {id(h)} and h = {h}')
 
 #tuple test (immutable object)
 i = (1,2,3)
 j = i
 #j[1] = 200  tuples are immutable --> results in a TypeError
 j = (3,4,5)
-print('Binding for j has changed from "i" to "j"--> id(i)=',id(i),i,'id(j)=',j)
+print(f'Binding for j has changed from {i} to {j} --> id(i) = {id(i)} and i = {i}, id(j) = {id(j)} and j = {j}')
 
 #dict test (mutable object)
 k = {'a':1, 'b':2, 'c':3}
 l = k
 l['b'] *= 100
-print('Binding for l is the same as the binding for l--> id(k)=',id(k), k,'id(l) =',id(l),l)
+print(f'Binding for k is the same as the binding for l--> id(k) = {id(k)} and k = {k}, id(l) = {id(l)} and l = {l}')
 
 print()
 print()
